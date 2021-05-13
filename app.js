ce5a4815a0bb1c368a6e208a3c1eaa3bb594d8a1
rajat77 <rajatseniwal1001@gmail.com>
2021-05-14 00:36:28 +0530

    Serving HTML, and static files through Express.JS

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
@@ -7,12 +8,14 @@ const adminRoutes = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-
-app.use(adminRoutes);
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use("/", (req, res, next) => {
-	res.status(404).send("<h1>404 : Page not found</h1>");
+	res
+		.status(404)
+		.sendFile(path.join(__dirname, "views", "page-not-found.html"));
 });
 
 app.listen(3000);
